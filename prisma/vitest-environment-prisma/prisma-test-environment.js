ee361cbcf7bd89a7aed93695c79d0542d8d68db8
Luiz Pires <lpiresjunior@gmail.com>
2023-08-27 22:48:34 -0300

    chore: config test environment

---
+++ prisma/vitest-environment-prisma/prisma-test-environment.js
@@ -0,0 +1,13 @@
+module.exports = {
+  transformMode: 'ssr',
+  name: 'prisma',
+  async setup() {
+    console.log('Setup')
+
+    return {
+      async teardown() {
+        console.log('Teardown')
+      },
+    }
+  },
+}
